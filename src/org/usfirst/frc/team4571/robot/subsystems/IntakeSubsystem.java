de33a712288f6e58fcdb47075b16c3e2028e3994
SharomG <marcalinerrocks@gmail.com>
2017-01-28 15:01:51 -0500

    added compressor

--- src/org/usfirst/frc/team4571/robot/subsystems/IntakeSubsystem.java
+++ src/org/usfirst/frc/team4571/robot/subsystems/IntakeSubsystem.java
@@ -1,10 +1,13 @@
- package org.usfirst.frc.team4571.robot.subsystems;
+package org.usfirst.frc.team4571.robot.subsystems;
 
 import org.usfirst.frc.team4571.robot.RobotConstants;
 import com.ctre.CANTalon;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,13 +17,19 @@ public class IntakeSubsystem extends Subsystem {
 	private CANTalon intakeMotor;
 	private DoubleSolenoid rollerSolenoid;
 	private boolean isRollerIn;
-public IntakeSubsystem(){
+	private Compressor compressor;
+	private Encoder encoder;
+
+	public IntakeSubsystem(){
 		this.intakeMotor = new CANTalon(RobotConstants.MOTOR_CHANNEL);
 		this.rollerSolenoid = new DoubleSolenoid(RobotConstants.ROLLER_FOWARD_SOLENOID_CHANNEL, RobotConstants.ROLLER_REVERSE_SOLENOID_CHANNEL);
+		this.compressor = new Compressor(0);
+		compressor.setClosedLoopControl(true);
+		this.encoder = new Encoder(RobotConstants.ENCODER_CHANNEL_A,
+				RobotConstants.ENCODER_CHANNEL_B, false,EncodingType.k4X);
 	}
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
-
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		//setDefaultCommand(new MySpecialCommand());
@@ -31,11 +40,11 @@ public IntakeSubsystem(){
 	public void out(){
 		this.rollerSolenoid.set(DoubleSolenoid.Value.kForward);
 		isRollerIn = false;
-    }
+	}
 	public void in(){
 		this.rollerSolenoid.set(DoubleSolenoid.Value.kReverse);
 		isRollerIn = true;
-    }
+	}
 	public Value getRollerSolenoidValue(){
 		return rollerSolenoid.get();
 	}
